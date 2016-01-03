d64e7fb444d0a203651188036a896dcc4f272f18
Ankit <ankit.ramesh.gupta@gmail.com>
2016-01-02 18:49:28 -0800

    Prob11

---
+++ Prob11/src/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int area = 0;
+
+        while (i < j) {
+            area = Math.max(area, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] > height[j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return area;
+    }
+}
